2ea6503b10cd569d1b0e5e83c1ea7dbe0926bfed
Chris Maltby <chris.maltby@gmail.com>
2020-12-09 19:06:12 +0000

    Add example sprite/avatar data

--- include/Sprite.h
+++ include/Sprite.h
@@ -5,6 +5,11 @@
 
 #define MAX_SPRITES 20
 
+typedef struct spritesheet_t {
+    UINT8 n_frames;
+    UINT8 frames[];
+} spritesheet_t;
+
 extern UINT8 sprites_pool[MAX_SPRITES];
 extern UINT8 sprites_pool_next;
 
---
+++ include/data/spritesheet_0.h
@@ -0,0 +1,11 @@
+#ifndef SPRITESHEET_0_H
+#define SPRITESHEET_0_H
+
+// SpriteSheet: 0
+
+#include "VM.h"
+
+extern const void __bank_spritesheet_0;
+extern const struct spritesheet_t spritesheet_0;
+
+#endif
