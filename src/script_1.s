6057b3a9570a8f879039ef045173c06a42ad908b
Chris Maltby <chris.maltby@gmail.com>
2020-12-16 14:23:24 +0000

    negative overlay speed to choose from global text in/out speed values

--- src/script_1.s
+++ src/script_1.s
@@ -9,6 +9,10 @@ ___bank_SCRIPT_1 = 4
 .globl ___bank_SCRIPT_1
 
 _SCRIPT_1::
+        ; Set text box open/close speed globally
+        VM_SET_CONST_INT8      _text_in_speed, 1
+        VM_SET_CONST_INT8      _text_out_speed, 2
+
         ; render multiple choice 
         VM_LOAD_TEXT            0
             .asciz "\0201) Option One\n2) Option Two"
@@ -16,7 +20,7 @@ _SCRIPT_1::
         ; Layout One Column ============== 
 
         ; slide in
-        VM_OVERLAY_MOVE_TO      0, 14, 1
+        VM_OVERLAY_MOVE_TO      0, 14, .OVERLAY_TEXT_IN_SPEED
 
         ; start displaying menu
         VM_DISPLAY_TEXT         0, 0, .UI_ENABLE_MENU_ONECOL                       ; no avatars, menu
@@ -25,8 +29,9 @@ _SCRIPT_1::
         VM_CHOICE               0, ^/(.UI_MENU_LAST_0 | .UI_MENU_CANCEL_B)/
 
         ; slide out
-        VM_OVERLAY_MOVE_TO      0, 18, 1
+        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, .UI_WAIT_WINDOW
+
 
         ; Layout Two Column ============== 
 
@@ -34,7 +39,7 @@ _SCRIPT_1::
             .asciz "\020One\nTwo\nThree\nFour\nFive\nSix"
 
         ; slide in
-        VM_OVERLAY_MOVE_TO      0, 12, 1
+        VM_OVERLAY_MOVE_TO      0, 12, .OVERLAY_TEXT_IN_SPEED
 
         ; start displaying text
         VM_DISPLAY_TEXT         0, 0, .UI_ENABLE_MENU_TWOCOL                       ; no avatars, menu
@@ -43,14 +48,14 @@ _SCRIPT_1::
         VM_CHOICE               0, ^/(.UI_MENU_LAST_0 | .UI_MENU_CANCEL_B)/
 
         ; slide out
-        VM_OVERLAY_MOVE_TO      0, 18, 1
+        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, .UI_WAIT_WINDOW
 
         ; Layout One Column Pin Right ============== 
 
         ; move window to 12 tiles by Y
         VM_OVERLAY_MOVE_TO      10, 18, 0
-        VM_OVERLAY_MOVE_TO      10, 10, 1
+        VM_OVERLAY_MOVE_TO      10, 10, .OVERLAY_TEXT_IN_SPEED
 
         ; start displaying text
         VM_DISPLAY_TEXT         0, 0, .UI_ENABLE_MENU_ONECOL                       ; no avatars, menu
@@ -59,7 +64,7 @@ _SCRIPT_1::
         VM_CHOICE               0, ^/(.UI_MENU_LAST_0 | .UI_MENU_CANCEL_B)/
 
         ; slide out
-        VM_OVERLAY_MOVE_TO      10, 18, 1
+        VM_OVERLAY_MOVE_TO      10, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, .UI_WAIT_WINDOW
 
         ; reset overlay position
@@ -70,7 +75,7 @@ _SCRIPT_1::
             .asciz "Hello World\nHello World"
 
         ; move window to 12 tiles by Y
-        VM_OVERLAY_MOVE_TO      0, 14, 1
+        VM_OVERLAY_MOVE_TO      0, 14, .OVERLAY_TEXT_IN_SPEED
 
         ; start displaying text
         VM_DISPLAY_TEXT         0, 0, 0    ; no avatar
@@ -79,7 +84,7 @@ _SCRIPT_1::
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
 
         ; slide out
-        VM_OVERLAY_MOVE_TO      0, 18, 1
+        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, .UI_WAIT_WINDOW
 
         ; Set slow text globally
@@ -90,7 +95,7 @@ _SCRIPT_1::
             .asciz "GLOBAL SLOW!!!\n\022Faster!!!"
 
         ; move window to 12 tiles by Y
-        VM_OVERLAY_MOVE_TO      0, 14, 1
+        VM_OVERLAY_MOVE_TO      0, 14, .OVERLAY_TEXT_IN_SPEED
 
         ; start displaying text
         VM_DISPLAY_TEXT         0, 0, 0    ; no avatar
@@ -99,7 +104,7 @@ _SCRIPT_1::
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
 
         ; slide out
-        VM_OVERLAY_MOVE_TO      0, 18, 1
+        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, .UI_WAIT_WINDOW
 
         ; Restore text speed globally
@@ -136,7 +141,7 @@ _SCRIPT_1::
         VM_POP                  9
 
         ; move window to 12 tiles by Y
-        VM_OVERLAY_MOVE_TO      0, 12, 1
+        VM_OVERLAY_MOVE_TO      0, 12, .OVERLAY_TEXT_IN_SPEED
 
         ; start displaying text
         VM_DISPLAY_TEXT         ___bank_spritesheet_1, _spritesheet_1, 0    ; avatar 1
@@ -145,14 +150,14 @@ _SCRIPT_1::
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
 
         ; slide out
-        VM_OVERLAY_MOVE_TO      0, 18, 1
+        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, .UI_WAIT_WINDOW
 
         ; show menu
         VM_OVERLAY_SETPOS       20, 9
         VM_LOAD_TEXT            0
             .asciz "\020option1\noption2\noption3\noption4\noption5\noption6\ncancel"
-        VM_OVERLAY_MOVE_TO      10, 9, 1
+        VM_OVERLAY_MOVE_TO      10, 9, .OVERLAY_TEXT_IN_SPEED
         VM_DISPLAY_TEXT         0, 0, .UI_ENABLE_MENU_ONECOL                ; no avatars, menu
 
         VM_PUSH                 0
@@ -164,7 +169,7 @@ _SCRIPT_1::
         VM_PUSH                 ^/(30*60)/      ; !!! always use escaping and brackets
         VM_INVOKE               b_wait_frames, _wait_frames, 1, .ARG0
 
-        VM_OVERLAY_MOVE_TO      20, 9, 1
+        VM_OVERLAY_MOVE_TO      20, 9, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_NONMODAL, .UI_WAIT_WINDOW
 
         VM_STOP
