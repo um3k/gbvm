a04c465b4a2738a70aeca0f661246285d4ad422e
Toxa <untoxa@mail.ru>
2020-12-31 15:24:35 +0300

    calling of some cleanup stuff on reset (please, check!)

--- src/main.c
+++ src/main.c
@@ -87,6 +87,10 @@ void process_VM() {
             case RUNNER_EXCEPTION: {
                 switch (vm_exception_code) {
                     case EXCEPTION_RESET: {
+                        // stop music
+                        music_stop();
+                        // cleanup core stuff
+                        events_init();  // is it a common stuff with change scene exception?
                         // clear all, including variables
                         script_runner_init(TRUE);
                         // load start scene
