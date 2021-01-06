592941882f9c3c20d3ebaff1ee29ac3a6cbd5497
Toxa <untoxa@mail.ru>
2021-01-07 00:50:59 +0300

    Ordnung!

--- src/data/script_s2_p_hit1.s
+++ src/data/script_s2_p_hit1.s
@@ -6,16 +6,26 @@
 ___bank_script_s2_p_hit1 = 255
 .globl ___bank_script_s2_p_hit1
 
+.globl ___bank_spritesheet_1, _spritesheet_1
+
 _script_s2_p_hit1::
 
         ; Text Dialogue
         VM_LOAD_TEXT            0
-        .asciz "Player hit\nCollision Group 1."
+        .asciz "STOP THAT MUSIC!\nCollision Group 1."
         VM_OVERLAY_MOVE_TO      0, 14, .OVERLAY_TEXT_IN_SPEED
         VM_DISPLAY_TEXT         0, 0, 0
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
         VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT)/
+
+        VM_MUSIC_STOP
+
+        ; Emote
+        VM_PUSH                 1
+        VM_PUSH                 ___bank_spritesheet_1
+        VM_PUSH                 _spritesheet_1
+        VM_INVOKE               b_vm_actor_emote, _vm_actor_emote, 3, .ARG2
 
         ; Stop Script
         VM_STOP
