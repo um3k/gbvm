e4df6c16823a82c9634b95030ed41562f60995e2
Chris Maltby <chris.maltby@gmail.com>
2021-01-08 14:02:37 +0000

    Add savepoint to scene_0

--- src/data/scene_0_sprites.c
+++ src/data/scene_0_sprites.c
@@ -7,11 +7,13 @@
 #include "data/spritesheet_1.h"
 #include "data/spritesheet_2.h"
 #include "data/spritesheet_3.h"
+#include "data/spritesheet_9.h"
 
 const void __at(255) __bank_scene_0_sprites;
 
 const far_ptr_t scene_0_sprites[] = {
   TO_FAR_PTR_T(spritesheet_1),
   TO_FAR_PTR_T(spritesheet_2),
-  TO_FAR_PTR_T(spritesheet_3)
+  TO_FAR_PTR_T(spritesheet_3),
+  TO_FAR_PTR_T(spritesheet_9)
 };
