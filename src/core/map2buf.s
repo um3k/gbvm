0b07740a461c5eac206a7cd211224d02ad475f48
Toxa <untoxa@mail.ru>
2021-01-15 00:12:59 +0300

    get_map_from_buf() 8-bit bounds overflow correction

--- src/core/map2buf.s
+++ src/core/map2buf.s
@@ -5,149 +5,155 @@
 ; void get_map_from_buf(UBYTE x, UBYTE y, UBYTE w, UBYTE h, unsigned char * dest, unsigned char * buf, UBYTE buf_w, UBYTE buf_h)
 
 _get_map_from_buf::
-        push    BC          ; bc, ret, x, y, w, h, dest, buf, buf_w, buf_h
+        push    bc          ; bc, ret, x, y, w, h, dest, buf, buf_w, buf_h
                             ; 0   2    4  5  6  7  8     10   12     13 
-        ldhl    SP, #4    
-        ld      D, (HL)     ; D = x
-        inc     HL
-        ld      E, (HL)     ; E = y
-        ldhl    SP, #9
-        ld      B, (HL)     ; BC = dest
-        dec     HL
-        ld      C, (HL)
-        dec     HL
-        ld      A, (HL-)    ; A = h
-        ld      H, (HL)     ; H = w
-        ld      L, A        ; L = h
+        ldhl    sp, #4    
+        ld      d, (hl)     ; d = x
+        inc     hl
+        ld      e, (hl)     ; e = y
+        ldhl    sp, #9
+        ld      b, (hl)     ; bc = dest
+        dec     hl
+        ld      c, (hl)
+        dec     hl
+        ld      a, (hl-)    ; a = h
+        ld      h, (hl)     ; h = w
+        ld      l, a        ; l = h
 
-        ld      A, H        ; check x bound
-        add     D
-        push    HL
-        ldhl    SP, #14     ; &buf_w
-        cp      (HL)        ; buf_w
-        jr      C, 8$
-        ld      A, D
-        cp      (HL)        ; buf_w
-        jp      NC, 9$
-        ld      A, (HL)     ; buf_w
-        sub     D
-        pop     HL
-        ld      H, A
+        ld      a, h        ; check x bound
+        add     d
+        jr      nc, 18$
+        ld      a, #0xff
+18$:
+        push    hl
+        ldhl    sp, #14     ; &buf_w
+        cp      (hl)        ; buf_w
+        jr      c, 8$
+        ld      a, d
+        cp      (hl)        ; buf_w
+        jp      nc, 9$
+        ld      a, (hl)     ; buf_w
+        sub     d
+        pop     hl
+        ld      h, a
         jr      1$
 8$:
-        pop     HL
+        pop     hl
 1$:
-        ld      A, L        ; check y bound
-        add     E
-        push    HL
-        ldhl    SP, #15     ; &buf_h
-        cp      (HL)        ; buf_h
-        jr      C, 10$
-        ld      A, E
-        cp      (HL)        ; buf_h
-        jp      NC, 9$
-        ld      A, (HL)     ; buf_h
-        sub     E
-        pop     HL
-        ld      L, A
+        ld      a, l        ; check y bound
+        add     e
+        jr      nc, 17$
+        ld      a, #0xff
+17$:
+        push    hl
+        ldhl    sp, #15     ; &buf_h
+        cp      (hl)        ; buf_h
+        jr      c, 10$
+        ld      a, e
+        cp      (hl)        ; buf_h
+        jp      nc, 9$
+        ld      a, (hl)     ; buf_h
+        sub     e
+        pop     hl
+        ld      l, a
         jr      7$
 10$:
-        pop     HL
+        pop     hl
 7$:
-        push    HL          ; Store WH
+        push    hl          ; store wh
         
-        ldhl    SP, #12     ; HL = origin
-        ld      A, (HL+)
-        ld      H, (HL)
-        ld      L, A
+        ldhl    sp, #12     ; hl = origin
+        ld      a, (hl+)
+        ld      h, (hl)
+        ld      l, a
 2$:
-        push    BC          ; Store dest
-        xor     A
-        or      E
-        jr      Z, 3$
+        push    bc          ; store dest
+        xor     a
+        or      e
+        jr      z, 3$
 
-        push    HL
-        ldhl    SP, #18     ; &buf_w
-        xor     A
-        ld      B, A
-        ld      C, (HL)     ; buf_w
+        push    hl
+        ldhl    sp, #18     ; &buf_w
+        xor     a
+        ld      b, a
+        ld      c, (hl)     ; buf_w
 
-        pop     HL
-11$:                        ; MUL BC by E and add reault to HL
-        srl     E
-        jr      NC, 12$
-        add     HL, BC
+        pop     hl
+11$:                        ; mul bc by e and add reault to hl
+        srl     e
+        jr      nc, 12$
+        add     hl, bc
 12$:
-        jr      Z, 3$       ; early exit
-        sla     C
-        rl      B
+        jr      z, 3$       ; early exit
+        sla     c
+        rl      b
         jr      11$
 3$:
-        ld      B, A
-        ld      C, D        ; BC = X coordinate
-        add     HL, BC
+        ld      b, a
+        ld      c, d        ; bc = x coordinate
+        add     hl, bc
 
-        pop     BC          ; BC = dest
-        pop     DE          ; DE = WH
+        pop     bc          ; bc = dest
+        pop     de          ; de = wh
         
-        push    HL          ; Store origin
-        push    DE          ; Store WH
-        push    BC
+        push    hl          ; store origin
+        push    de          ; store wh
+        push    bc
 16$:
-        srl     D
-        jr      NC, 14$
+        srl     d
+        jr      nc, 14$
 
-        ld      A, (HL+)    ; copy one byte
-        ld      (BC), A
-        inc     BC
+        ld      a, (hl+)    ; copy one byte
+        ld      (bc), a
+        inc     bc
 14$:
-        jr      Z, 15$
+        jr      z, 15$
 5$:
-        ld      A, (HL+)    ; copy two bytes
-        ld      (BC), A
-        inc     BC
-        ld      A, (HL+)
-        ld      (BC), A
-        inc     BC
+        ld      a, (hl+)    ; copy two bytes
+        ld      (bc), a
+        inc     bc
+        ld      a, (hl+)
+        ld      (bc), a
+        inc     bc
         
-        dec     D
-        jr      NZ,5$
+        dec     d
+        jr      nz,5$
 15$:
-        pop     BC
+        pop     bc
         
-        pop     HL          ; HL = WH
-        ld      D,H         ; Restore D = W
-        pop     HL          ; HL = origin
-        dec     E
-        jr      Z,6$
+        pop     hl          ; hl = wh
+        ld      d,h         ; restore d = w
+        pop     hl          ; hl = origin
+        dec     e
+        jr      z,6$
 
-        push    HL          ; Next line
-        ldhl    SP, #14   
-        ld      A, (HL)     ; buf_w
-        pop     HL
-        add     L
-        ld      L, A
-        ld      A, H
+        push    hl          ; next line
+        ldhl    sp, #14   
+        ld      a, (hl)     ; buf_w
+        pop     hl
+        add     l
+        ld      l, a
+        ld      a, h
         adc     #0
-        ld      H, A
+        ld      h, a
         
-        push    HL
-        ldhl    SP, #8
-        ld      L, (HL)
+        push    hl
+        ldhl    sp, #8
+        ld      l, (hl)
         
-        ld      H, #0
-        add     HL, BC
-        ld      B, H
-        ld      C, L
-        pop     HL
-        push    HL          ; Store current origin
+        ld      h, #0
+        add     hl, bc
+        ld      b, h
+        ld      c, l
+        pop     hl
+        push    hl          ; store current origin
 
-        push    DE          ; Store WH
-        push    BC
+        push    de          ; store wh
+        push    bc
         
         jr      16$
 9$:
-        pop     HL                 
+        pop     hl                 
 6$:
-        pop     BC
+        pop     bc
         ret
