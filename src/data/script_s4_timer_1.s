dfdff42c357061c658d4d1dc7c6f43d2b98f2d18
Toxa <untoxa@mail.ru>
2021-01-27 19:29:44 +0300

    script variable intersection

--- src/data/script_s4_timer_1.s
+++ src/data/script_s4_timer_1.s
@@ -8,19 +8,19 @@ ___bank_script_s4_timer1 = 255
 .globl ___bank_tileset_6, _tileset_6
 
 _script_s4_timer1::
-        VM_IF_CONST     .EQ     VAR_S2_FLOWER_STATE, 0, 1$, 0
-        VM_IF_CONST     .EQ     VAR_S2_FLOWER_STATE, 1, 2$, 0
+        VM_IF_CONST     .EQ     VAR_S4_FLOWER_STATE, 0, 1$, 0
+        VM_IF_CONST     .EQ     VAR_S4_FLOWER_STATE, 1, 2$, 0
 
         VM_REPLACE_TILE         0x56, ___bank_tileset_6, _tileset_6, 3, 1
-        VM_SET_CONST            VAR_S2_FLOWER_STATE, 0
+        VM_SET_CONST            VAR_S4_FLOWER_STATE, 0
         VM_STOP
 
 1$:
         VM_REPLACE_TILE         0x56, ___bank_tileset_6, _tileset_6, 4, 1
-        VM_SET_CONST            VAR_S2_FLOWER_STATE, 1
+        VM_SET_CONST            VAR_S4_FLOWER_STATE, 1
         VM_STOP
 
 2$:
         VM_REPLACE_TILE         0x56, ___bank_tileset_6, _tileset_6, 5, 1
-        VM_SET_CONST            VAR_S2_FLOWER_STATE, 2
+        VM_SET_CONST            VAR_S4_FLOWER_STATE, 2
         VM_STOP
