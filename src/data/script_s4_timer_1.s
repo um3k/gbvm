7069ab70f2b5716ad8105a511174b6b274ff20ef
Toxa <untoxa@mail.ru>
2021-01-29 21:13:48 +0300

    fix flowers and complicate trampoline script

--- src/data/script_s4_timer_1.s
+++ src/data/script_s4_timer_1.s
@@ -11,16 +11,16 @@ _script_s4_timer1::
         VM_IF_CONST     .EQ     VAR_S4_FLOWER_STATE, 0, 1$, 0
         VM_IF_CONST     .EQ     VAR_S4_FLOWER_STATE, 1, 2$, 0
 
-        VM_REPLACE_TILE         0x56, ___bank_tileset_6, _tileset_6, 3, 1
+        VM_REPLACE_TILE         0xA7, ___bank_tileset_6, _tileset_6, 3, 1
         VM_SET_CONST            VAR_S4_FLOWER_STATE, 0
         VM_STOP
 
 1$:
-        VM_REPLACE_TILE         0x56, ___bank_tileset_6, _tileset_6, 4, 1
+        VM_REPLACE_TILE         0xA7, ___bank_tileset_6, _tileset_6, 4, 1
         VM_SET_CONST            VAR_S4_FLOWER_STATE, 1
         VM_STOP
 
 2$:
-        VM_REPLACE_TILE         0x56, ___bank_tileset_6, _tileset_6, 5, 1
+        VM_REPLACE_TILE         0xA7, ___bank_tileset_6, _tileset_6, 5, 1
         VM_SET_CONST            VAR_S4_FLOWER_STATE, 2
         VM_STOP
