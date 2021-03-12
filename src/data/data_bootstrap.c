9ffe10debe2e013b2bc28debf72e24e03619a39f
Chris Maltby <chris.maltby@gmail.com>
2021-03-12 20:42:01 +0000

    Set engine field initial values using an init function in data_bootstrap, move field headers into relevant parts of the engine

--- src/data/data_bootstrap.c
+++ src/data/data_bootstrap.c
@@ -12,16 +12,19 @@ const far_ptr_t start_player_palette = TO_FAR_PTR_T(palette_0);
 const UBYTE start_player_move_speed = 16;
 const UBYTE start_player_anim_tick = 7;
 const far_ptr_t ui_fonts[] = {TO_FAR_PTR_T(vwf_font), TO_FAR_PTR_T(vwf_font_bold)};
-WORD plat_min_vel = 304;
-WORD plat_walk_vel = 6400;
-WORD plat_climb_vel = 4000;
-WORD plat_run_vel = 10496;
-WORD plat_walk_acc = 152;
-WORD plat_run_acc = 228;
-WORD plat_dec = 208;
-WORD plat_jump_vel = 16384;
-WORD plat_grav = 1792;
-WORD plat_hold_grav = 512;
-WORD plat_max_fall_vel = 20000;
-UBYTE topdown_grid = 8;
-UBYTE fade_style = 1;
+
+void bootstrap_init() __banked {
+    plat_min_vel = 304;
+    plat_walk_vel = 6400;
+    plat_climb_vel = 4000;
+    plat_run_vel = 10496;
+    plat_walk_acc = 152;
+    plat_run_acc = 228;
+    plat_dec = 208;
+    plat_jump_vel = 16384;
+    plat_grav = 1792;
+    plat_hold_grav = 512;
+    plat_max_fall_vel = 20000;
+    topdown_grid = 8;
+    fade_style = 1;    
+}
