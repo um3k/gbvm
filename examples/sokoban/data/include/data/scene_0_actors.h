23fb880b96f3538b5e491c8b182a4930673f0a10
Chris Maltby <chris.maltby@gmail.com>
2021-03-25 19:59:54 +0000

    Add sokoban example

---
+++ examples/sokoban/data/include/data/scene_0_actors.h
@@ -0,0 +1,12 @@
+#ifndef SCENE_0_ACTORS_H
+#define SCENE_0_ACTORS_H
+
+// Scene: Level 1
+// Actors
+
+#include "gbs_types.h"
+
+extern const void __bank_scene_0_actors;
+extern const struct actor_t scene_0_actors[];
+
+#endif
---
+++ examples/sokoban/data/src/data/scene_0_sprites.c
@@ -0,0 +1,15 @@
+#pragma bank 255
+
+// Scene: Level 1
+// Sprites
+
+#include "gbs_types.h"
+#include "data/spritesheet_0.h"
+#include "data/spritesheet_1.h"
+
+const void __at(255) __bank_scene_0_sprites;
+
+const far_ptr_t scene_0_sprites[] = {
+    TO_FAR_PTR_T(spritesheet_0),
+    TO_FAR_PTR_T(spritesheet_1)
+};
