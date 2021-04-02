70a4a1215b0fb612c5d337eb764d8798044a7b2c
Chris Maltby <chris.maltby@gmail.com>
2021-04-02 11:58:57 +0100

    Rerun Sokoban codegen: remove unused scene colors data

--- examples/sokoban/data/src/data/scene_0.c
+++ examples/sokoban/data/src/data/scene_0.c
@@ -5,7 +5,6 @@
 #include "gbs_types.h"
 #include "data/background_0.h"
 #include "data/scene_0_collisions.h"
-#include "data/scene_0_colors.h"
 #include "data/palette_0.h"
 #include "data/palette_1.h"
 #include "data/spritesheet_0.h"
@@ -20,7 +19,6 @@ const struct scene_t scene_0 = {
     .type = SCENE_TYPE_TOPDOWN,
     .background = TO_FAR_PTR_T(background_0),
     .collisions = TO_FAR_PTR_T(scene_0_collisions),
-    .colors = TO_FAR_PTR_T(scene_0_colors),
     .palette = TO_FAR_PTR_T(palette_0),
     .sprite_palette = TO_FAR_PTR_T(palette_1),
     .player_sprite = TO_FAR_PTR_T(spritesheet_0),
