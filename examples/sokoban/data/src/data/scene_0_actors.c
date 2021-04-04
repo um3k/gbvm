83f0dda782267b8001a7e963e8a2da2bda6f33d7
Chris Maltby <chris.maltby@gmail.com>
2021-04-04 12:48:44 +0100

    Add all boxes to Sokoban test level, no solution check to show Win state yet

--- examples/sokoban/data/src/data/scene_0_actors.c
+++ examples/sokoban/data/src/data/scene_0_actors.c
@@ -8,6 +8,16 @@
 #include "data/script_s0a0_interact.h"
 #include "data/spritesheet_1.h"
 #include "data/script_s0a1_interact.h"
+#include "data/spritesheet_1.h"
+#include "data/script_s0a2_interact.h"
+#include "data/spritesheet_1.h"
+#include "data/script_s0a3_interact.h"
+#include "data/spritesheet_1.h"
+#include "data/script_s0a4_interact.h"
+#include "data/spritesheet_1.h"
+#include "data/script_s0a5_interact.h"
+#include "data/spritesheet_1.h"
+#include "data/script_s0a6_interact.h"
 
 const void __at(255) __bank_scene_0_actors;
 
@@ -15,7 +25,7 @@ const struct actor_t scene_0_actors[] = {
     {
         // Box,
         .pos = {
-            .x = 80 * 16,
+            .x = 64 * 16,
             .y = 40 * 16
         },
         .bounds = {
@@ -36,8 +46,8 @@ const struct actor_t scene_0_actors[] = {
     {
         // Box,
         .pos = {
-            .x = 80 * 16,
-            .y = 88 * 16
+            .x = 32 * 16,
+            .y = 104 * 16
         },
         .bounds = {
             .left = 0,
@@ -53,5 +63,110 @@ const struct actor_t scene_0_actors[] = {
         .collision_group = COLLISION_GROUP_NONE,
         .collision_enabled = TRUE,
         .script = TO_FAR_PTR_T(script_s0a1_interact)
+    },
+    {
+        // Box,
+        .pos = {
+            .x = 80 * 16,
+            .y = 56 * 16
+        },
+        .bounds = {
+            .left = 0,
+            .bottom = 7,
+            .right = 15,
+            .top = -8
+        },
+        .dir = DIR_DOWN,
+        .sprite = TO_FAR_PTR_T(spritesheet_1),
+        .move_speed = 16,
+        .anim_tick = 3,
+        .pinned = FALSE,
+        .collision_group = COLLISION_GROUP_NONE,
+        .collision_enabled = TRUE,
+        .script = TO_FAR_PTR_T(script_s0a2_interact)
+    },
+    {
+        // Box,
+        .pos = {
+            .x = 80 * 16,
+            .y = 72 * 16
+        },
+        .bounds = {
+            .left = 0,
+            .bottom = 7,
+            .right = 15,
+            .top = -8
+        },
+        .dir = DIR_DOWN,
+        .sprite = TO_FAR_PTR_T(spritesheet_1),
+        .move_speed = 16,
+        .anim_tick = 3,
+        .pinned = FALSE,
+        .collision_group = COLLISION_GROUP_NONE,
+        .collision_enabled = TRUE,
+        .script = TO_FAR_PTR_T(script_s0a3_interact)
+    },
+    {
+        // Box,
+        .pos = {
+            .x = 64 * 16,
+            .y = 104 * 16
+        },
+        .bounds = {
+            .left = 0,
+            .bottom = 7,
+            .right = 15,
+            .top = -8
+        },
+        .dir = DIR_DOWN,
+        .sprite = TO_FAR_PTR_T(spritesheet_1),
+        .move_speed = 16,
+        .anim_tick = 3,
+        .pinned = FALSE,
+        .collision_group = COLLISION_GROUP_NONE,
+        .collision_enabled = TRUE,
+        .script = TO_FAR_PTR_T(script_s0a4_interact)
+    },
+    {
+        // Box,
+        .pos = {
+            .x = 80 * 16,
+            .y = 104 * 16
+        },
+        .bounds = {
+            .left = 0,
+            .bottom = 7,
+            .right = 15,
+            .top = -8
+        },
+        .dir = DIR_DOWN,
+        .sprite = TO_FAR_PTR_T(spritesheet_1),
+        .move_speed = 16,
+        .anim_tick = 3,
+        .pinned = FALSE,
+        .collision_group = COLLISION_GROUP_NONE,
+        .collision_enabled = TRUE,
+        .script = TO_FAR_PTR_T(script_s0a5_interact)
+    },
+    {
+        // Box,
+        .pos = {
+            .x = 96 * 16,
+            .y = 104 * 16
+        },
+        .bounds = {
+            .left = 0,
+            .bottom = 7,
+            .right = 15,
+            .top = -8
+        },
+        .dir = DIR_DOWN,
+        .sprite = TO_FAR_PTR_T(spritesheet_1),
+        .move_speed = 16,
+        .anim_tick = 3,
+        .pinned = FALSE,
+        .collision_group = COLLISION_GROUP_NONE,
+        .collision_enabled = TRUE,
+        .script = TO_FAR_PTR_T(script_s0a6_interact)
     }
 };
---
+++ examples/sokoban/data/src/data/script_s0a3_interact.s
@@ -0,0 +1,77 @@
+.include "vm.i"
+.include "data/game_globals.i"
+
+.area _CODE_255
+
+ACTOR = -4
+
+___bank_script_s0a3_interact = 255
+.globl ___bank_script_s0a3_interact
+
+_script_s0a3_interact::
+        VM_LOCK
+
+        ; Local Actor
+        VM_PUSH_CONST           0
+        VM_PUSH_CONST           0
+        VM_PUSH_CONST           0
+        VM_PUSH_CONST           0
+
+        ; Actor Set Active
+        VM_SET_CONST            ACTOR, 4
+        VM_ACTOR_ACTIVATE       ACTOR
+
+        ; Actor Push
+        VM_SET_CONST            ACTOR, 0
+        VM_PUSH_CONST           0
+        VM_ACTOR_GET_DIR        ^/(ACTOR - 1)/, .ARG0
+        VM_SET_CONST            ^/(ACTOR - 1)/, 4
+        VM_ACTOR_GET_POS        ^/(ACTOR - 1)/
+        VM_IF_CONST .EQ         .ARG0, .DIR_UP, 1$, 0
+        VM_IF_CONST .EQ         .ARG0, .DIR_LEFT, 2$, 0
+        VM_IF_CONST .EQ         .ARG0, .DIR_RIGHT, 3$, 0
+        VM_RPN
+            .R_REF      ^/(ACTOR + 2 - 1)/
+            .R_INT16    256
+            .R_OPERATOR .ADD
+            .R_STOP
+        VM_SET                  ^/(ACTOR + 2 - 2)/, .ARG0
+        VM_POP                  1
+        VM_JUMP                 4$
+1$:
+        VM_RPN
+            .R_REF      ^/(ACTOR + 2 - 1)/
+            .R_INT16    256
+            .R_OPERATOR .SUB
+            .R_INT16    0
+            .R_OPERATOR .MAX
+            .R_STOP
+        VM_SET                  ^/(ACTOR + 2 - 2)/, .ARG0
+        VM_POP                  1
+        VM_JUMP                 4$
+2$:
+        VM_RPN
+            .R_REF      ^/(ACTOR + 1 - 1)/
+            .R_INT16    256
+            .R_OPERATOR .SUB
+            .R_INT16    0
+            .R_OPERATOR .MAX
+            .R_STOP
+        VM_SET                  ^/(ACTOR + 1 - 2)/, .ARG0
+        VM_POP                  1
+        VM_JUMP                 4$
+3$:
+        VM_RPN
+            .R_REF      ^/(ACTOR + 1 - 1)/
+            .R_INT16    256
+            .R_OPERATOR .ADD
+            .R_STOP
+        VM_SET                  ^/(ACTOR + 1 - 2)/, .ARG0
+        VM_POP                  1
+4$:
+        VM_POP                  1
+        VM_SET_CONST            ^/(ACTOR + 3)/, .ACTOR_ATTR_CHECK_COLL
+        VM_ACTOR_MOVE_TO        ACTOR
+
+        ; Stop Script
+        VM_STOP
