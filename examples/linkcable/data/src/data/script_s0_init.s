a14a1128c31df17ea5422cee9c658d2634a8bf13
Chris Maltby <chris.maltby@gmail.com>
2021-04-06 22:00:05 +0100

    Better linkcable script

--- examples/linkcable/data/src/data/script_s0_init.s
+++ examples/linkcable/data/src/data/script_s0_init.s
@@ -1,8 +1,6 @@
 .include "vm.i"
 .include "data/game_globals.i"
 
-.globl b_wait_frames, _wait_frames
-
 .area _CODE_255
 
 
@@ -13,12 +11,8 @@ ___bank_script_s0_init = 255
 _script_s0_init::
         VM_LOCK
 
-        ; Wait 1 Frame
-        VM_PUSH_CONST           1
-        VM_INVOKE               b_wait_frames, _wait_frames, 1, .ARG0
-
         ; Fade In
-        VM_FADE_IN              1
+        VM_FADE_IN              2
 
         ; Text Multiple Choice
         VM_LOAD_TEXT            0
@@ -35,20 +29,11 @@ _script_s0_init::
 
         ; If Variable True
         VM_IF_CONST .EQ         VAR_S0_IS_MASTER, 1, 1$, 0
-        ; Wait 1 Frame
-        VM_PUSH_CONST           1
-        VM_INVOKE               b_wait_frames, _wait_frames, 1, .ARG0
-
-        ; Fade In
-        VM_FADE_IN              1
-
         VM_SIO_SET_MODE         .SIO_MODE_SLAVE
-        ; Wait 1 Frame
-        VM_PUSH_CONST           1
-        VM_INVOKE               b_wait_frames, _wait_frames, 1, .ARG0
-
-        ; Fade In
-        VM_FADE_IN              1
+        VM_JUMP                 2$
+1$:
+        VM_SIO_SET_MODE         .SIO_MODE_MASTER
+2$:
 
         ; Text Multiple Choice
         VM_LOAD_TEXT            0
@@ -74,50 +59,6 @@ _script_s0_init::
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
         VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT)/
-
-        VM_JUMP                 2$
-1$:
-        ; Wait 1 Frame
-        VM_PUSH_CONST           1
-        VM_INVOKE               b_wait_frames, _wait_frames, 1, .ARG0
-
-        ; Fade In
-        VM_FADE_IN              1
-
-        VM_SIO_SET_MODE         .SIO_MODE_MASTER
-        ; Wait 1 Frame
-        VM_PUSH_CONST           1
-        VM_INVOKE               b_wait_frames, _wait_frames, 1, .ARG0
-
-        ; Fade In
-        VM_FADE_IN              1
-
-        ; Text Multiple Choice
-        VM_LOAD_TEXT            0
-        .asciz "\001\001 1\n 0"
-        VM_OVERLAY_CLEAR        0, 0, 20, 4, .UI_COLOR_WHITE, .UI_DRAW_FRAME
-        VM_OVERLAY_MOVE_TO      0, 14, .OVERLAY_TEXT_IN_SPEED
-        VM_DISPLAY_TEXT
-        VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT)/
-        VM_CHOICE               VAR_S0_MY_VALUE, ^/(.UI_MENU_LAST_0 | .UI_MENU_CANCEL_B)/, 2
-            .MENUITEM           1, 1, 0, 0, 0, 2
-            .MENUITEM           1, 2, 0, 0, 1, 0
-        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
-        VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT)/
-
-        VM_SIO_EXCHANGE         VAR_S0_MY_VALUE, VAR_S0_THEIR_VALUE, 1
-        ; Text Dialogue
-        VM_LOAD_TEXT            2
-        .dw VAR_S0_MY_VALUE, VAR_S0_THEIR_VALUE
-        .asciz "HOST GOT:\nMINE=%d\nTHEIRS=%d"
-        VM_OVERLAY_CLEAR        0, 0, 20, 5, .UI_COLOR_WHITE, .UI_DRAW_FRAME
-        VM_OVERLAY_MOVE_TO      0, 13, .OVERLAY_TEXT_IN_SPEED
-        VM_DISPLAY_TEXT
-        VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
-        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
-        VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT)/
-
-2$:
 
         ; Stop Script
         VM_STOP
