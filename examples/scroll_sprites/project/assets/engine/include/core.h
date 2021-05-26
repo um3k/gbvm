1bd1e044ad293dcbf122c332f752407c76d375c8
Toxa <untoxa@mail.ru>
2021-05-26 12:30:53 +0300

    remove engine copy from examples

--- examples/scroll_sprites/project/assets/engine/include/core.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CORE_H_INCLUDE
-#define _CORE_H_INCLUDE
-
-#include <gb/gb.h>
-
-void core_reset() __banked;
-void core_run() __banked;
-
-#endif
--- examples/scroll_sprites/project/assets/engine/include/states/logo.h
+++
@@ -1,9 +0,0 @@
-#ifndef STATE_LOGO_H
-#define STATE_LOGO_H
-
-#include <gb/gb.h>
-
-void logo_init() __banked;
-void logo_update() __banked;
-
-#endif
