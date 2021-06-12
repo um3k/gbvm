b2d2ce96aa7e10bd6a8d8467f4e27a74fc0f3f1a
Chris Maltby <chris.maltby@gmail.com>
2021-06-12 18:03:50 +0100

    Regenerate custom_scripts example with latest gbstudio

--- examples/custom_scripts/data/src/data/script_s0_init.s
+++ examples/custom_scripts/data/src/data/script_s0_init.s
@@ -5,8 +5,6 @@
 
 .area _CODE_255
 
-
-
 ___bank_script_s0_init = 255
 .globl ___bank_script_s0_init
 
@@ -22,8 +20,8 @@ _script_s0_init::
 
         ; Text Dialogue
         VM_LOAD_TEXT            0
-        .asciz "Before Call\n"
-        VM_OVERLAY_CLEAR        0, 0, 20, 4, .UI_COLOR_WHITE, .UI_DRAW_FRAME
+        .asciz "Before Call"
+        VM_OVERLAY_CLEAR        0, 0, 20, 4, .UI_COLOR_WHITE, ^/(.UI_AUTO_SCROLL | .UI_DRAW_FRAME)/
         VM_OVERLAY_MOVE_TO      0, 14, .OVERLAY_TEXT_IN_SPEED
         VM_DISPLAY_TEXT
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
@@ -35,8 +33,8 @@ _script_s0_init::
 
         ; Text Dialogue
         VM_LOAD_TEXT            0
-        .asciz "After Call\n"
-        VM_OVERLAY_CLEAR        0, 0, 20, 4, .UI_COLOR_WHITE, .UI_DRAW_FRAME
+        .asciz "After Call"
+        VM_OVERLAY_CLEAR        0, 0, 20, 4, .UI_COLOR_WHITE, ^/(.UI_AUTO_SCROLL | .UI_DRAW_FRAME)/
         VM_OVERLAY_MOVE_TO      0, 14, .OVERLAY_TEXT_IN_SPEED
         VM_DISPLAY_TEXT
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
