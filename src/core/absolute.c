04959f0dadb729e1e20ccce93e897b79b17b4095
Toxa <untoxa@mail.ru>
2021-07-12 16:05:01 +0300

    struct sizes checking at compile time

--- src/core/absolute.c
+++ src/core/absolute.c
@@ -1,7 +1,16 @@
 #include "gbs_types.h"
 
-OAM_item_t __at(0xDF00) shadow_OAM2[40];        // 160 bytes
-palette_entry_t __at(0xDFA0) BkgPalette[8];     // 64 bytes
-UBYTE __at(0xDFE0) vwf_tile_data[16 * 2];       // 32 bytes
-                                                // --------
-                                                // 256 bytes total
+#define SHADOW_OAM_BASE_ADDRESS 0xDF00
+
+#define add_check__(a,b) add_check___(a,b)
+#define add_check___(a,b) check_##a##_##b
+#define check_size(typ,sz) typedef char add_check__(typ,__LINE__)[ (sizeof(typ) == (sz)) ? 1 : -1]
+
+OAM_item_t      __at(SHADOW_OAM_BASE_ADDRESS)                                           shadow_OAM2[40];        // 160 bytes
+check_size(shadow_OAM2, 160);
+palette_entry_t __at(SHADOW_OAM_BASE_ADDRESS + sizeof(shadow_OAM2))                     BkgPalette[8];          // 64 bytes
+check_size(BkgPalette, 64);
+UBYTE           __at(SHADOW_OAM_BASE_ADDRESS + sizeof(shadow_OAM2) +sizeof(BkgPalette)) vwf_tile_data[16 * 2];  // 32 bytes
+check_size(vwf_tile_data, 32);
+                                                                                                                // --------
+                                                                                                                // 256 bytes total
