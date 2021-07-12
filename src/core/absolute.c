1f2552a81d66763d1aa1037f77bf4ab304879269
Toxa <untoxa@mail.ru>
2021-07-12 15:46:55 +0300

    comments about structure sizes in absolute.c

--- src/core/absolute.c
+++ src/core/absolute.c
@@ -1,5 +1,7 @@
 #include "gbs_types.h"
 
-OAM_item_t __at(0xDF00) shadow_OAM2[40];
-palette_entry_t __at(0xDFA0) BkgPalette[8];
-UBYTE __at(0xDFE0) vwf_tile_data[16 * 2];
+OAM_item_t __at(0xDF00) shadow_OAM2[40];        // 160 bytes
+palette_entry_t __at(0xDFA0) BkgPalette[8];     // 64 bytes
+UBYTE __at(0xDFE0) vwf_tile_data[16 * 2];       // 32 bytes
+                                                // --------
+                                                // 256 bytes total
