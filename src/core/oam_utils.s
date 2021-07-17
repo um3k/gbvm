b51ed71f85649624e105a18ee0e2ed4945788ae2
Toxa <untoxa@mail.ru>
2021-07-18 00:13:57 +0300

    better comments for hide_sprites_range()

--- src/core/oam_utils.s
+++ src/core/oam_utils.s
@@ -33,12 +33,11 @@ _hide_hardware_sprites::
         jr      nc, 0$
 
         ld      (hl), d
-        add     hl, de
+        add     hl, de          ; carry is reset here because hl can't overflow
 
         ret     z               ; z is not affected by 16-bit add
-
 0$:
-        rra                     ; carry is never set here, because of ret c above
+        rra                     ; carry is never set here
         jr      nc, 1$
 
         ld      (hl), d
@@ -48,7 +47,6 @@ _hide_hardware_sprites::
         add     hl, de
 
         ret     z               ; z is not affected by 16-bit add
-
 1$:
         ld      (hl), d
         add     hl, de
