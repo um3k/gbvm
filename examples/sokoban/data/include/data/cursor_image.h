63e67e77834b5b11ffc11cadfa1a03ca3ff7bd59
Chris Maltby <chris.maltby@gmail.com>
2021-09-05 19:56:14 +0100

    Recompile examples/sokoban with latest gbstudio

--- examples/sokoban/data/include/data/cursor_image.h
+++ examples/sokoban/data/include/data/cursor_image.h
@@ -5,7 +5,7 @@
 
 #include "gbs_types.h"
 
-extern const void __bank_cursor_image;
+BANKREF_EXTERN(cursor_image)
 extern const unsigned char cursor_image[];
 
 #endif
--- examples/sokoban/data/include/data/spritesheet_0.h
+++ examples/sokoban/data/include/data/spritesheet_0.h
@@ -5,7 +5,7 @@
 
 #include "gbs_types.h"
 
-extern const void __bank_spritesheet_0;
+BANKREF_EXTERN(spritesheet_0)
 extern const struct spritesheet_t spritesheet_0;
 
 #endif
